d6baee2737eacc48623e9151979ab9d3886af0ef
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2023-12-24 00:26:00 +0700

    Create document.cpp

---
+++ document.cpp
@@ -0,0 +1,10 @@
+#include "document.h"
+
+using namespace std;
+
+void PrintDocument(const Document& document) {
+    cout << "{ "s
+         << "document_id = "s << document.id << ", "s
+         << "relevance = "s << document.relevance << ", "s
+         << "rating = "s << document.rating << " }"s << endl;
+}
